7097682ca02b6f6199354f98a7b1494e4fcbc6c7
Damrongsiri, Prisa (Agoda) <prisa.damrongsiri@agoda.com>
2017-07-02 23:43:37 +0700

    fixed build failed

--- src/main/kotlin/com/omise/api/entity/Donation.kt
+++ src/main/kotlin/com/omise/api/entity/Donation.kt
@@ -4,12 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 data class Donation(
 
-        @com.google.gson.annotations.SerializedName("customer_name")
+        @SerializedName("customer_name")
         var name: String = "",
 
-        @com.google.gson.annotations.SerializedName("card_token")
+        @SerializedName("card_token")
         var token: String = "",
 
-        @com.google.gson.annotations.SerializedName("donation_amount")
+        @SerializedName("donation_amount")
         var amount: Double = 0.00
 )
